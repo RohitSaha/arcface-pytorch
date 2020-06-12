ea4be29a3007bd0338eafc4b279b37f775dd6681
rohit_saha <rohit@modiface.com>
2020-06-12 02:05:07 +0000

    adding correct paths

--- arcface_inference.py
+++ arcface_inference.py
@@ -175,39 +175,90 @@ if __name__ == '__main__':
     print('Loaded model checkpoint')
     model.eval()
 
-    BASE_DIR = '~/modidatasets/VoxCeleb2/preprocessed_data/test'
-    ids = os.listdir(BASE_DIR)
-    ids = [os.path.join(BASE_DIR, idx) for idx in ids]
+    '''Directory:
+    real images: ~/modidatasets/VoxCeleb2/preprocessed_data/test/ids/identity'
+    synthesized images: 
+        ~/synthesized_images/
+            <id_1>/
+                <id_1_2>/
+                    img_n
+                <id_1_3>/
+                    img_n
+            <id_2>/
+                <id_2_1>/
+                    img_n
+                <id_2_3>/
+                    img_n
+    descriptors_files:
+        ~/descriptors_files/
+            <id_1>/
+                id_1.pkl
+                id_1_2.pkl
+                id_1_3.pkl
+            <id_2>/
+                id_2.pkl
+                id_2_1.pkl
+                id_2_3.pkl
+
+
+    Read from original images
+    Read from synthesized directory
+    Save to pickle folder
+    '''
+    DESC_DIR = '~/desciptors_files'
+    REAL_DIR = '~/modidatasets/VoxCeleb2/preprocessed_data/test/ids/identity'
+
+    ids = os.listdir(REAL_DIR)
 
     for idx in ids:
-        frames = os.listdir(idx)
-        frames = [os.path.join(idx, frame) for frame in frames]
-        frames = [frame for frame in frames if 'continuous_frame' in frame]
+        idx_path = os.path.join(REAL_DIR, idx)
+        codes = os.listdir(idx_path)
+        for code in codes:
+            code_path = os.path.join(idx_path, code)
+            mp4s = os.listdir(code_path)
+            for mp4 in mp4s:
+                mp4_path = os.path.join(code_path, mp4)
+                frames = os.listdir(mp4_path)
 
-        # there should be at most 32 frames
-        features = voxceleb2_test(model, frames, 32) # 32 x 1024
-        features = np.mean(features, axis=0)
-        
-        # img_paths = ['/mnt/Data/Data/modidatasets/VoxCeleb2/train_processed/id01185/VhkwMUvywiQ/00046/9_continuous_frame.png']
-        # voxceleb2_test(model, img_paths, 32)
+                frames = [os.path.join(mp4_path, frame) for frame in frames]
+                frames = [frame for frame in frames if 'random_frame' in frame]
 
-        with open('{}_averaged_decriptor.pkl'.format(idx), 'wb') as handle:
-            pickle.dump(features, handle)
+                # there should be at most 32 frames
+                features = voxceleb2_test(model, frames, 32) # 32 x 1024
+                features = np.mean(features, axis=0)
+                
+                # img_paths = ['/mnt/Data/Data/modidatasets/VoxCeleb2/train_processed/id01185/VhkwMUvywiQ/00046/9_continuous_frame.png']
+                # voxceleb2_test(model, img_paths, 32)
+                save_dir = os.path.join(DESC_DIR, '{}'.format(idx), '{}.pkl'.format(idx))
+                if not os.path.exists(save_dir):
+                    os.makedirs(save_dir)
+
+                with open(save_dir, 'wb') as handle:
+                    pickle.dump(features, handle)
 
     print('Averaged decriptors extracted')
 
-    BASE_DIR = '~/path to synthesized images'
-    ids = os.listdir(BASE_DIR)
-    ids = [os.path.join(BASE_DIR, idx) for idx in ids]
+    SYNTH_DIR = '~/synthesized_images' 
+    ids = os.listdir(SYNTH_DIR)
 
     for idx in ids:
-        frames = os.listdir(idx)
-        frames = [os.path.join(idx, frame) for frame in frames]
-        
-        features = voxceleb2_test(model, frames, 32)
-        
-        with open('{}_synthesized_descriptors.pkl'.format(idx), 'wb') as handle:
-            pickle.dump(features, handle)
+        idx_path = os.path.join(SYNTH_DIR, idx)
+        sub_ids = os.listdir(idx_path)
+
+        for sub_idx in sub_ids:
+            sub_idx_path = os.path.join(idx_path, sub_idx)
+            frames = os.listdir(sub_idx_path)
+            frames = [os.path.join(sub_idx_path, frame) for frame in frames]
+            frames = [frame for frame in frames if 'random_frame' in frame]
+
+            features = voxceleb2_test(model, frames, 32)
+            
+            save_dir = os.path.join(SYNTH_DIR, '{}'.format(idx), '{}.pkl'.format(sub_idx))
+            if not os.path.exists(save_dir):
+                os.makedirs(save_dir)
+
+            with open(save_dir, 'wb') as handle:
+                pickle.dump(features, handle)
 
     print('Descriptors computed for synthesized images')    
 
