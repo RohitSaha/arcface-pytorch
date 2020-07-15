773aead43af72ea7a9a1c5a0d1c393ade58ed0bc
rohit_saha <rohit@modiface.com>
2020-07-15 20:30:25 +0000

    results uploaded as a zip

--- upload.py
+++ upload.py
@@ -4,20 +4,16 @@ from dotenv import load_dotenv
 
 from modiml.logging.mlflow_logger import MLflowLogger
 
-image_dir = '/home/ubuntu/arcface-pytorch/synthesized_images'
-descriptor_dir = '/home/ubuntu/arcface-pytorch/descriptors'
-landmark_dir = '/home/ubuntu/arcface-pytorch/landmark'
-
-CELEB_IDX = 'id00017' 
-mlflow_params = {"celeb_id": CELEB_IDX}
+results = '/home/ubuntu/results.zip'
+params = {'model name': 'rohit-26',}
+headers = ['none']
 
 mlflow_logger = MLflowLogger(
         "Neural Talk Head - 2",
-        'images-{}'.format(CELEB_IDX),
-        params=mlflow_params,
+        'rohit-26-results',
+        params=params,
+        metrics_headers=headers,
 )
 load_dotenv(".env")
 
-mlflow.log_artifacts(image_dir, "Synthesized_images")
-mlflow.log_artifacts(descriptor_dir, "Descriptors")
-mlflow.log_artifacts(landmark_dir, "Landmarks")
+mlflow.log_artifact(results, "Results")
